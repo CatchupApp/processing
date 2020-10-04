c49139a0e68f7f2092d8bd670354ca9b2f27be8d
Michael Fatemi <myfatemi04@gmail.com>
2020-10-04 12:34:24 -0400

    Added direct video transcription

--- Keywords/main.py
+++ Keywords/main.py
@@ -4,11 +4,13 @@ if 'GOOGLE_APPLICATION_CREDENTIALS' not in os.environ:
     # os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '../../credentials.json'
     pass
 
-from audio_to_text import transcribe_file
-from google.cloud import storage
-from io import BytesIO
+# from audio_to_text import transcribe_file
+# from google.cloud import storage
+from google.cloud import videointelligence
+# from io import BytesIO
 import mongo
-import wave
+import text_to_keywords
+# import wave
 
 def validate_message(message, param):
     var = message.get(param)
@@ -25,26 +27,60 @@ def process_audio(file, context):
     bucket = validate_message(file, "bucket")
     name = validate_message(file, "name")
 
-    client = storage.Client()
+    print("Starting transcription for", bucket, " / " , name)
 
-    bucket = client.get_bucket('catchup-app')
-    blob = storage.Blob(name, bucket)
+    video_client = videointelligence.VideoIntelligenceServiceClient()
+    
+    # maybe add other features here?
+    features = [videointelligence.enums.Feature.SPEECH_TRANSCRIPTION]
 
-    output = BytesIO()
+    config = videointelligence.types.SpeechTranscriptionConfig(
+        language_code="en-US", enable_automatic_punctuation=True
+    )
 
-    client.download_blob_to_file(blob, output)
-    output.seek(0)
+    video_context = videointelligence.types.VideoContext(
+        speech_transcription_config=config
+    )
 
-    with wave.open(output) as f:
-        channels = f.getnchannels()
+    operation = video_client.annotate_video(
+        input_uri=f"gs://{bucket}/{name}", features=features, video_context=video_context
+    )
 
-    output.seek(0)
+    transcriptions = []
 
-    response = transcribe_file(output, channels)
-    response = [paragraph.to_json() for paragraph in response]
+    annotation_results = operation.result(timeout=600)
+    first_result = annotation_results.annotation_results[0]
+    speech_transcriptions_raw = first_result.speech_transcriptions
 
-    mongo.set_keyword_data(file["name"], response)
+    for result in speech_transcriptions_raw:
+        alternative = result.alternatives[0]
+        transcript = alternative.transcript
+        confidence = alternative.confidence
+
+        words = []
+        for word_info in alternative.words:
+            word = word_info.word
+            start = word_info.start_time.seconds + 1e-9 * word_info.start_time.nanos
+            end = word_info.end_time.seconds + 1e-9 * word_info.end_time.nanos
+
+            words.append({"word": word, "start": start, "end": end})
+
+        keywords = text_to_keywords.get_keywords(transcript)
+
+        transcriptions.append({
+            "transcript": transcript,
+            "confidence": confidence,
+            "words": words,
+            "keywords": keywords
+        })
+
+    # response = transcribe_file(output, channels)
+    # response = [paragraph.to_json() for paragraph in response]
+
+    mongo.set_keyword_data(file["name"], transcriptions)
 
     print("File {} processed.".format(file["name"]))
 
-# process_audio({"bucket": "catchup-app", "name": "audio/test.wav"}, {})
+    return transcriptions
+
+# process_audio({"bucket": "catchup-app", "name": "test_video.mp4"}, {})
--- Keywords/mongo.py
+++ Keywords/mongo.py
@@ -7,13 +7,13 @@ client = pymongo.MongoClient(os.environ['MONGO_URL'])
 db = client['catchup']
 
 # collection level
-catchup = db['catchup']
+catchup = db['videos']
 
 def set_keyword_data(source, keywords_data):
     query = {"source": source}
 
     if not catchup.find_one(query):
-        catchup.insert_one({"source": source, "keywords": keywords_data})
+        catchup.insert_one({"source": source, "transcription": keywords_data})
     else:
-        update = {"$set": {"keywords": keywords_data}}
+        update = {"$set": {"transcription": keywords_data}}
         catchup.update_one(query, update)
