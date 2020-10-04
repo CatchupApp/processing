a5261b468a5b650dccf377e4972d067c9a9e8396
SuhasNandiraju <47066425+SuhasNandiraju@users.noreply.github.com>
2020-10-04 13:27:55 -0400

    Update Flaskapi.py

--- Flaskapi.py
+++ Flaskapi.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
-from summarizer import summarize
-from mathocr import *
+from Summarizer.summarizer import summarize
+from other_functions.mathocr import *
 
 app = Flask(__name__)
 
