76f3f2fb393b927860ffd9c49abb50e967d698ce
Michael Fatemi <myfatemi04@gmail.com>
2020-10-04 12:47:33 -0400

    Fixed Mongo forking error

--- Keywords/mongo.py
+++ Keywords/mongo.py
@@ -1,19 +1,15 @@
 import pymongo
 import os
 
-client = pymongo.MongoClient(os.environ['MONGO_URL'])
-
-# database level
-db = client['catchup']
-
-# collection level
-catchup = db['videos']
+client = pymongo.MongoClient(os.environ['MONGO_URL'], connect=False)
 
 def set_keyword_data(source, keywords_data):
+    videos = (client['catchup'])['videos']
+
     query = {"source": source}
 
-    if not catchup.find_one(query):
-        catchup.insert_one({"source": source, "transcription": keywords_data})
+    if not videos.find_one(query):
+        videos.insert_one({"source": source, "transcription": keywords_data})
     else:
         update = {"$set": {"transcription": keywords_data}}
-        catchup.update_one(query, update)
+        videos.update_one(query, update)
